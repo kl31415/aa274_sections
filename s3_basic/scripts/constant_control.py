cc733bb0c75c29ce96dccf02d1046e389b8a1724
Peyton Lee <peyton.lee.14122@gmail.com>
2024-10-10 18:19:35 -0700

    section 3 changes added

---
+++ s3_basic/scripts/constant_control.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
+
+class ConstantControl(Node):
+    def __init__(self) -> None:
+        super().__init__("constant_control")
+        self.get_logger().info("Publisher has been created!")
+        self.publisher = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.subscriber = self.create_subscription(Bool, '/kill', self.kill_callback, 10)
+        self.timer = self.create_timer(0.2, self.control_callback)
+
+    def control_callback(self):
+        msg = Twist()
+        msg.linear.x = float(10)
+        msg.angular.z = float(0)
+        self.publisher.publish(msg)
+
+    def kill_callback(self, msg):
+        if msg.data:
+            self.timer.cancel()
+            stop = Twist()
+            stop.linear.x = 0.0
+            stop.angular.z = 0.0
+            self.publisher.publish(stop)
+
+def main(args=None):
+    rclpy.init(args=args)
+    publisher = ConstantControl()
+    rclpy.spin(publisher)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
+    
